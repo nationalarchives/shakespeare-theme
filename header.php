aafbc067b4edf1c246bf5dce4fcfef3d639bc8a9
SDWilkes <simonwilkes@gmail.com>
2016-01-08 11:41:26 +0000

    Improving zoom on smartphones

--- header.php
+++ header.php
@@ -77,10 +77,12 @@
                         <li <?php if (is_front_page()){?>class="active"<?php }?>><a href="/">About the exhibition</a></li>
                         <li <?php if (is_page_template("will.php")){?>class="active"<?php }?>><a href="/shakespeares-will/">Shakespeare's will</a></li>
 
+                        <?php if(get_page_by_title('a life in writing')) : ?>
 
 
 
-                        <li <?php if (is_page_template("documents.php") or is_page_template("document-viewer.php")){?>class="active"<?php }?>><a href="/a-life-in-writing/">A life in writing</a></li>
+
+                        <li <?php if (is_page_template("documents.php") or is_page_template("document-viewer.php")){?>class="active"<?php }?>><a href="/a-life-in-writing/">A life in writing</a></li>  <?php endif;?>
                     </ul>
 
                 </div>
